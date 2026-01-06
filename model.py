f64cc0e901f6aeca8d3ac3ffc1a11f1d2e620ab5
HARSAVARDAN88 <geethasaravanakumar33@gmail.com>
2026-01-06 21:29:09 +0530

    Add scikit-learn dependency

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
+# Load dataset (CSV must be in same folder)
 df = pd.read_csv("bike_loan_decision_tree.csv")
 
 X = df[['income', 'age', 'credit_score']]
 y = df['loan_approved']
 
-model = DecisionTreeClassifier()
+# Train model
+model = DecisionTreeClassifier(random_state=42)
 model.fit(X, y)
 
 def predict_loan(income, age, credit):
-    return model.predict([[income, age, credit]])[0]
+    return int(model.predict([[income, age, credit]])[0])
